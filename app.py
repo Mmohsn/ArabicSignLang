bcb982960c590281fd5d549c6147ac3d1382da7e
Mohamed Mohsen <46654744+Mmohsn@users.noreply.github.com>
2024-05-19 09:40:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -80,5 +80,6 @@ def predict():
         return jsonify({'error': str(e)})
 
     
-if __name__ == '__main__':
-    app.run(port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port)
